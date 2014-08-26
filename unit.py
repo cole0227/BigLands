65c13b2738ea793799dd8ab5d4478430d6469322
cole0227 <cole0227@algonquinlive.com>
2014-08-26 15:13:09 -0400

    cleaned up a bit

--- unit.py
+++ unit.py
@@ -91,7 +91,7 @@ class Unit(object):
 
         string = "Level:"+str(self.m_level)
         string += "\t\tHP: "+str(self.m_attribs["health"])
-        string += " "+str(self.m_attribs["armour"])+"A "+str(self.m_attribs["dodge_chance"])+"D"
+        string += " "+str(self.m_attribs["armour"])+"A "+str(self.m_attribs["dodge_chance"])+"D "+str(self.m_attribs["health_regeneration"])+"R"
         string += "\t\tSpeed: "+str(self.m_attribs["movement_speed"])+"%"
         string += "\t\tAttack: "+str(self.m_attribs["attack_damage"])+"/"+str(self.m_attribs["attack_speed"])+"%"
         string += "\t\tCrits: "+str(100+self.m_attribs["critical_damage"].get())+"/"+str(self.m_attribs["critical_chance"])+"%"
@@ -169,6 +169,7 @@ class Actor_Unit(Unit):
         if "constitution" in self.m_attribs:
 
             self.m_attribs["health"].add(self.get("constitution")*9)
+            self.m_attribs["health_regeneration"].add(self.get("constitution")*0.05)
 
         if "intelligence" in self.m_attribs:
 
@@ -178,6 +179,7 @@ class Actor_Unit(Unit):
         if "wisdom" in self.m_attribs:
 
             self.m_attribs["attack_damage"].add(0.5*self.get("wisdom"))
+            self.m_attribs["health_regeneration"].add(self.get("wisdom")*0.02)
 
         if "charisma" in self.m_attribs:
 
@@ -251,13 +253,35 @@ class Actor_Unit(Unit):
 def random_bonus(mini,maxi,count):
     u = Unit({})
     for i in range(0,count):
-        u.merge(Unit({random.choice(("health","armour","attack_speed","attack_damage","movement_speed","critical_chance","critical_damage","dodge_chance","strength","dexterity","constitution","intelligence","wisdom","charisma")):(random.randint(mini,maxi),0)}))
+
+        #this list contains all the heroic stats 3 times
+        stat = random.choice(("health","health_regeneration","armour","attack_speed","attack_damage","movement_speed","critical_chance","critical_damage","dodge_chance","strength","dexterity","constitution","intelligence","wisdom","charisma","strength","dexterity","constitution","intelligence","wisdom","charisma","strength","dexterity","constitution","intelligence","wisdom","charisma"))
+
+        if(stat == "health"):
+            u.merge(Unit({stat:(random.randint(mini*3,maxi*3),0)}))
+        elif(stat == "health_regeneration"):
+            u.merge(Unit({stat:(random.randint(mini/20.0,maxi/20.0),0)}))
+        elif("chance" in stat):
+            u.merge(Unit({stat:(random.randint(mini/5.0,maxi/5.0),0)}))
+        else:
+            u.merge(Unit({stat:(random.randint(mini,maxi),0)}))
     return u
 
 def random_bonus_combat(mini,maxi,count):
     u = Unit({})
     for i in range(0,count):
-        u.merge(Unit({random.choice(("health","armour","attack_speed","attack_damage","movement_speed","critical_chance","critical_damage","dodge_chance")):(random.randint(mini,maxi),0)}))
+
+        #this list contains no heroic stats
+        stat = random.choice(("health","health_regeneration","armour","attack_speed","attack_damage","movement_speed","critical_chance","critical_damage","dodge_chance"))
+
+        if(stat == "health"):
+            u.merge(Unit({stat:(random.randint(mini*3,maxi*3),0)}))
+        elif(stat == "health_regeneration"):
+            u.merge(Unit({stat:(random.randint(mini/20.0,maxi/20.0),0)}))
+        elif("chance" in stat):
+            u.merge(Unit({stat:(random.randint(mini/5.0,maxi/5.0),0)}))
+        else:
+            u.merge(Unit({stat:(random.randint(mini,maxi),0)}))
     return u
 
 def random_bonus_heroic(mini,maxi,count):
@@ -268,11 +292,8 @@ def random_bonus_heroic(mini,maxi,count):
 
 def random_armour_piece(armour,bonus_count=0,level=0,mini=20,maxi=30):
     u = Unit({"armour":(random.randint(armour*0.8,armour*1.2),10)},level)
-    if(bonus_count > 2):
-        u.merge(random_bonus_heroic(mini,maxi,bonus_count-2))
-        u.merge(random_bonus_combat(mini,maxi,2))
-    else:
-        u.merge(random_bonus_combat(mini,maxi,bonus_count))
+
+    u.merge(random_bonus(mini,maxi,bonus_count))
 
     for k in u.m_attribs.keys():
         u.m_attribs[k].merge(attribute(u,0,1,0))
@@ -281,15 +302,15 @@ def random_armour_piece(armour,bonus_count=0,level=0,mini=20,maxi=30):
 
 if __name__ == '__main__':
 
-    source_unit_nme          = Actor_Unit({"health":(80,1),  "armour":(8,0.1),  "attack_speed":100, "attack_damage":(40,1), "movement_speed":100, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_knight       = Actor_Unit({"health":(115,1), "armour":(20,0.1), "attack_speed":70,  "attack_damage":(40,1), "movement_speed":80 , "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_warden       = Actor_Unit({"health":(100,1), "armour":(12,0.1), "attack_speed":90,  "attack_damage":(40,1), "movement_speed":100, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_zealot       = Actor_Unit({"health":(90,1),  "armour":(8,0.1),  "attack_speed":90,  "attack_damage":(44,1), "movement_speed":110, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_savage       = Actor_Unit({"health":(100,1), "armour":(8,0.1),  "attack_speed":90,  "attack_damage":(40,1), "movement_speed":120, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_friar        = Actor_Unit({"health":(150,1), "armour":(4,0.1),  "attack_speed":70,  "attack_damage":(28,1), "movement_speed":120, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_warlock      = Actor_Unit({"health":(100,1), "armour":(4,0.1),  "attack_speed":80,  "attack_damage":(61,1), "movement_speed":80 , "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_ranger       = Actor_Unit({"health":(80,1),  "armour":(8,0.1),  "attack_speed":110, "attack_damage":(40,1), "movement_speed":110, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
-    source_unit_scout        = Actor_Unit({"health":(100,1), "armour":(4,0.1),  "attack_speed":115, "attack_damage":(47,1), "movement_speed":130, "critical_chance":50, "critical_damage":0, "dodge_chance":0})
+    source_unit_nme          = Actor_Unit({"health":(80,1),  "health_regeneration":(1,0.05), "armour":(8,0.1),  "attack_speed":100, "attack_damage":(40,1), "movement_speed":100, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_knight       = Actor_Unit({"health":(115,1), "health_regeneration":(1,0.05), "armour":(20,0.1), "attack_speed":70,  "attack_damage":(40,1), "movement_speed":80 , "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_warden       = Actor_Unit({"health":(100,1), "health_regeneration":(1,0.05), "armour":(12,0.1), "attack_speed":90,  "attack_damage":(40,1), "movement_speed":100, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_zealot       = Actor_Unit({"health":(90,1),  "health_regeneration":(1,0.05), "armour":(8,0.1),  "attack_speed":90,  "attack_damage":(44,1), "movement_speed":110, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_savage       = Actor_Unit({"health":(100,1), "health_regeneration":(1,0.05), "armour":(8,0.1),  "attack_speed":90,  "attack_damage":(40,1), "movement_speed":120, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_friar        = Actor_Unit({"health":(150,1), "health_regeneration":(1,0.05), "armour":(4,0.1),  "attack_speed":70,  "attack_damage":(28,1), "movement_speed":120, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_warlock      = Actor_Unit({"health":(100,1), "health_regeneration":(1,0.05), "armour":(4,0.1),  "attack_speed":80,  "attack_damage":(61,1), "movement_speed":80 , "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_ranger       = Actor_Unit({"health":(80,1),  "health_regeneration":(1,0.05), "armour":(8,0.1),  "attack_speed":110, "attack_damage":(40,1), "movement_speed":110, "critical_chance":0, "critical_damage":0, "dodge_chance":0})
+    source_unit_scout        = Actor_Unit({"health":(100,1), "health_regeneration":(1,0.05), "armour":(4,0.1),  "attack_speed":115, "attack_damage":(47,1), "movement_speed":130, "critical_chance":50, "critical_damage":0, "dodge_chance":0})
     #source_unit_scout.append(Unit({"armour":8}))
 
     source_class_shepherd   = Unit({"strength":(1,1.0), "dexterity":(1,1.0), "constitution":(1,1.0), "intelligence":(1,1.0), "wisdom":(3,1.8), "charisma":(3,1.8)})
@@ -305,30 +326,28 @@ if __name__ == '__main__':
     source_class_ambassador = Unit({"strength":(1,0.9), "dexterity":(1,0.9), "constitution":(1,0.9), "intelligence":(1,0.9), "wisdom":(1,0.9), "charisma":(5,2.9)})
 
     monster_race_beast = Unit({"strength":(5,3.3), "dexterity":(3,1.5), "constitution":(5,2.9), "critical_chance":20, "critical_damage":50})
-    monster_race_demon    = Unit({"strength":(3,1.7), "dexterity":(1,0.4), "constitution":(3,1.8), "intelligence":(3,2.1), "wisdom":(8,2.0), "charisma":(9,3.0)})
-    monster_race_giant    = Unit({"strength":(9,2.8), "dexterity":(1,1.0), "constitution":(7,1.9), "intelligence":(6,0.4), "wisdom":(1,0.8), "charisma":(1,0.4), "attack_damage":30, "attack_speed":-30,})
+    monster_race_cultist = Unit({"strength":(1,1.1), "dexterity":(0,0.4), "constitution":(1,1.2), "intelligence":(3,1.4), "wisdom":(2,1.4), "charisma":(9,3.0)})
+    monster_race_demon = Unit({"strength":(3,1.7), "dexterity":(0,0.4), "constitution":(3,1.8), "intelligence":(3,2.1), "wisdom":(8,2.0), "charisma":(9,3.0)})
+    monster_race_giant = Unit({"strength":(9,2.8), "dexterity":(1,1.0), "constitution":(7,1.9), "intelligence":(6,0.4), "wisdom":(1,0.8), "charisma":(1,0.4), "attack_damage":30, "attack_speed":-30,})
     monster_race_frost_giant = Unit({"armour":15, "health":50}).merge(monster_race_giant)
     monster_race_fire_giant = Unit({"attack_damage":15, "health":80}).merge(monster_race_giant)
     monster_race_golem = Unit({"strength":(1,1.8), "constitution":(3,1.8), "wisdom":(3,1.8)})
-    monster_race_lizard    = Unit({"strength":(1,0.7), "dexterity":(4,2.4), "constitution":(1,0.8), "intelligence":(3,2.1), "wisdom":(7,2.6), "dodge_chance":15})
-    monster_race_lizard_dragon    = Unit({"dodge_chance":(5,0.34)}).merge(monster_race_lizard)
+    monster_race_lizard = Unit({"strength":(1,0.7), "dexterity":(4,2.4), "constitution":(1,0.8), "intelligence":(3,2.1), "wisdom":(7,2.6), "dodge_chance":15})
+    monster_race_lizard_dragon = Unit({"dodge_chance":(5,0.34)}).merge(monster_race_lizard)
     monster_race_undead = Unit({"strength":(13,1.9), "dexterity":(1,1.0), "constitution":(2,1.5), "intelligence":(1,1.0)})
     monster_race_ghost = Unit({"intelligence":(4,2.5)}).merge(monster_race_undead)
     monster_race_zombie = Unit({"strength":(4,2.5),"constitution":(14,4.0)}).merge(monster_race_undead)
-    monster_race_skeleton = Unit({"dexterity":(2,2.0),"armour":20}).merge(monster_race_undead)
+    monster_race_skeleton = Unit({"dexterity":(2,2.0),"armour":20}).merge(monster_race_undead)    
 
     source_unit_knight.merge(source_class_monk_river)
     source_unit_warlock.merge(source_class_brigand)
     source_unit_nme.merge(source_class_settler)
     source_unit_scout.merge(source_class_moonbrain)
 
-    source_unit_nme.recalc_bonus()
-
     #what's a suit of armour worth?
-    #item_armour = random_armour_piece(300,5,0)
-    #item_armour.level_up(2)
-    #print item_armour.output_attribs()
-    #source_unit_nme.append(item_armour)
+    item_armour = random_armour_piece(100,0,0)
+    print item_armour.output_attribs()
+    source_unit_knight.append(item_armour)
 
     # add the fatty to see very long battles
     #unit_fatty = Unit({"health":10000})
@@ -347,8 +366,8 @@ if __name__ == '__main__':
     player2 = source_unit_warlock.copy()
 
     for i in range(0,1):
-        player1.level_up(1)
-        player2.level_up(1)
+        player1.level_up(250)
+        player2.level_up(250)
         print player1
         print player2,"\n"
 
